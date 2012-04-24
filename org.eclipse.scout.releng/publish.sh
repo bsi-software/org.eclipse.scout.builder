4642feb8a49373a1a1a4f6f96eb8fd66903126db
Judith Gull <jgu@bsiag.com>
2012-04-24 07:29:48 +0000

    Scout Build: publish script adjusted for composite nightly repository.

--- org.eclipse.scout.releng/publish.sh
+++ org.eclipse.scout.releng/publish.sh
@@ -4,7 +4,6 @@ repositoriesDir=$workingDir
 stageTriggerFileName=doStage
 
 
-
 processZipFile()
 {
   backupDir=$(pwd)
@@ -16,27 +15,45 @@ processZipFile()
     unzip $zipFile -d $stagingArea/working >$stagingArea/NUL
     chgrp -R technology.scout $stagingArea/working
     chmod -R g+w $stagingArea/working
-     
+
     cd $stagingArea/working
-    for d in {[0-9\.]*,nightly}
-    do
-      if [ -d "$d" ]; then
-        if  [ -d  $repositoriesDir/$d""_new ]; then
-           rm -rf $repositoriesDir/$d""_new
-        fi
-        mv $stagingArea/working/$d $repositoriesDir/$d""_new
-        # backup original
-        if [ -d $repositoriesDir/$d ]; then
-          if [ -d $repositoriesDir/$d""_backup ]; then
-            rm -rf $repositoriesDir/$d""_backup
+      for d in {[0-9\.]*,nightly}
+     do
+        if [ -d "$d" ]; then
+           if  [ -d  $repositoriesDir/$d""_new ]; then
+              rm -rf $repositoriesDir/$d""_new
           fi
-          mv $repositoriesDir/$d $repositoriesDir/$d""_backup
+          mv $stagingArea/working/$d $repositoriesDir/$d""_new
+          # backup original
+          if [ -d $repositoriesDir/$d ]; then
+            if [ -d $repositoriesDir/$d""_backup ]; then
+              rm -rf $repositoriesDir/$d""_backup
+            fi
+            cp -r $repositoriesDir/$d $repositoriesDir/$d""_backup
+          fi
+          # copy new repository
+          cp -f $repositoriesDir/$d""_new/*.xml $repositoriesDir/$d
+          cp -rf $repositoriesDir/$d""_new/N* $repositoriesDir/$d
+          rm -rf $repositoriesDir/$d""_new
         fi
-        mv $repositoriesDir/$d""_new $repositoriesDir/$d
-      fi
-    done
-    cp -f $stagingArea/working/* $repositoriesDir/
-    rm -rf $stagingArea/working
+     done
+
+      ## remove old nightly repositories that are not contained in the composite updateiste
+      cd $workingDir/nightly
+      for sub in N*
+      do
+        if [ -d "$sub" ]; then
+          if ! (grep -q "$sub" compositeContent.xml);
+            then
+             echo "$sub is not contained in composite. Removing..";
+             rm -rf $sub
+          fi
+        fi
+      done
+
+     #cleanup stagingArea
+     cp -n $stagingArea/working/* $repositoriesDir/
+     rm -rf $stagingArea/working
     cd $backupDir
   else
     echo "md5 not valid for $zipFile!"
@@ -48,11 +65,10 @@ if [ -f $stagingArea/$stageTriggerFileName ]; then
   cd $stagingArea
   mv $stagingArea/$stageTriggerFileName $stagingArea/processing
   processZipFile $(md5sum -c $stagingArea/processing)
-  rm -rf $stagingArea/* 
+  rm -rf $stagingArea/*
   cd $backupDir
 fi
 
 #echo $stagingArea/stage.zip
 #username=$(ls -l $stagingArea/stage.zip | awk '{print $3}')
 #if [ "$username" == "aho" ]; then
-
